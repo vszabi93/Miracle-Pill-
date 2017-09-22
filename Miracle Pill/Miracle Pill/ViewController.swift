7f250d5847d555b9c0f6048630f56e234d22480a
vszabi93 <31307298+vszabi93@users.noreply.github.com>
2017-09-22 09:06:12 +0200

    First App
    My first App

---
+++ Miracle Pill/Miracle Pill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle Pill
+//
+//  Created by Varga Szabolcs on 2017. 09. 22..
+//  Copyright © 2017. Varga Szabolcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
